03ed78bc23d2214fd86b005f217dbbe4e3018476
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-11-21 11:42:20 +0100

    setting domain

--- backend-java/PXLNews/post-service/src/main/java/be/pxl/services/domain/Post.java
+++ backend-java/PXLNews/post-service/src/main/java/be/pxl/services/domain/Post.java
@@ -1,4 +1,20 @@
 package be.pxl.services.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }
--- backend-java/PXLNews/review-service/src/main/java/be/pxl/services/domain/dto/ReviewRequest.java
+++ backend-java/PXLNews/review-service/src/main/java/be/pxl/services/domain/dto/ReviewRequest.java
@@ -1,4 +1,13 @@
 package be.pxl.services.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewRequest {
 }
