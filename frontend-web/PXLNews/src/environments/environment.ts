e78a321217dc8e699cb0212000b60f180539196f
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2025-01-12 19:26:42 +0100

    editing dockerfile to do build

--- frontend-web/PXLNews/src/environments/environment.ts
+++ frontend-web/PXLNews/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrlPost: 'APIURL' + '/post/post',
-    apiUrlReview: 'APIURL' + '/review/review',
-    apiUrlComment: 'APIURL' + '/comment/comment',
+    apiUrlPost: 'http://localhost:8085/post/post',
+    apiUrlReview: 'http://localhost:8085/review/review',
+    apiUrlComment: 'http://localhost:8085/comment/comment',
 };
